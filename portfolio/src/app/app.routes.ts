8edef3fee2a94ebb3cb23dd49bd48959712c7463
jr2025 <rakotondravalyj@gmail.com>
2025-09-10 00:42:49 +0200

    first modif

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+title:'Accueil',
+loadComponent: () => import('./page/accueil/accueil.component').then(m => m.AccueilComponent),
+path:'accueil',
+
+   } 
+
+];
